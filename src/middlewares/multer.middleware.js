aa8c6a65249513744511f3b2e7b85eac32dcbe5e
Shashwat <shashwatw.1906@gmail.com>
2025-01-09 15:32:37 +0530

    initiate project

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,18 @@
+import multer from 'multer';
+
+// Set up storage engine
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/temp');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+// Initialize upload
+const upload = multer({
+    storage: storage
+})
+
+export {upload}
