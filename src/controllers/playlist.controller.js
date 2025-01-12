0d0a1e4987f351270717e99c407f20e0121029f1
Shashwat Wawge <shashwatw.1906@gmail.com>
2025-01-12 11:58:04 +0530

    create tweet controller

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,7 +3,7 @@ import {asyncHandler} from '../utils/asyncHandler.js'
 import {Video} from '../models/video.model.js'
 import {ApiError} from '../utils/ApiError.js'
 import {ApiResponse} from '../utils/ApiResponse.js'
-import {User} from '../models/user.model.js'
+
 // create playlist
 const createPlayList = asyncHandler(async(req,res)=>{
     const {name , description , videoId} = req.body
